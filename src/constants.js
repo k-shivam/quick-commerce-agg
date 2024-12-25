c79eca0518b9552734bb773fb55aed0017bcc9fa
k-shivam <42085548+k-shivam@users.noreply.github.com>
2024-12-25 21:17:06 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,50 @@ const quantityMapping = {
   MILLILITRE: "ml",
 };
 
+const mergeProductData = (data) => {
+  const productMap = {};
+  data.forEach((platformData) => {
+    platformData.forEach((product) => {
+      const {
+        brand,
+        name,
+        price,
+        discountedPrice,
+        quantity,
+        etaInfo,
+        platform,
+      } = product;
+      const productKey = `${name}-${brand}`;
+      if (productMap[productKey]) {
+        productMap[productKey][platform] = {
+          price,
+          discountedPrice,
+          quantity,
+          etaInfo,
+        };
+      } else {
+        productMap[productKey] = {
+          name,
+          brand,
+          [platform]: {
+            price,
+            discountedPrice,
+            quantity,
+            etaInfo,
+          },
+        };
+      }
+    });
+  });
+
+  const result = Object.values(productMap).filter((product) => {
+    return Object.keys(product).length > 2;
+  });
+  result.sort((a, b) => a.name.localeCompare(b.name));
+  return result;
+};
+
 module.exports = {
   quantityMapping,
+  mergeProductData,
 };
